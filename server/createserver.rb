a42573dca4fbbb0e9f050a88179ac620cdb8c095
Vjeux <vjeuxx@gmail.com>
2012-01-13 19:53:26 +0100

    Ruby server

--- server/createserver.rb
+++ server/createserver.rb
@@ -2,23 +2,20 @@ require 'rubygems'
 require 'cool.io'
 require_relative 'listener.rb'
 
-class Server
-	def create(addr, port, callback)
-
-		cool.io.server addr, port do
-			on_connect do
-				callback.call(self)
-			end
-
-			on_close do
-				Listener.call(self, 'close')
-			end
-
-			on_read do |data|
-				Listener.call(self, 'data', data)
-			end
+def createServer(addr, port, callback)
+	cool.io.server addr, port do
+		on_connect do
+			callback.call(self)
 		end
 
-		cool.io.run
+		on_close do
+			Listener.call(self, 'close')
+		end
+
+		on_read do |data|
+			Listener.call(self, 'data', data)
+		end
 	end
+
+	cool.io.run
 end
--- server/server.rb
+++ server/server.rb
@@ -3,44 +3,35 @@ require_relative 'createserver.rb'
 ADDR = '127.0.0.1'
 PORT = 1337
 
-class GameTheory
-	def initialize()
-		
+client_id = 0
+clients = []
 
-		client_id = 0
-		clients = []
+createServer(ADDR, PORT, lambda { |socket|
 
-		Server.new.create(ADDR, PORT, lambda { |socket|
+	client = {"id" => client_id, "socket" => socket, "alive" => true}
+	client_id += 1
+	Listener.add(socket, 'close', lambda {
+		client.alive = false
+	})
+	clients << client
+	a = [client]
+	sendMessage("vjeux", "Welcome! Please wait for a new game to start.")
 
-			client = {"id" => client_id, "socket" => socket, "alive" => true}
-			client_id += 1
-			Listener.add(socket, 'close', lambda {
-				client.alive = false
-			})
-			clients << client
-			a = [client]
-			sendMessage("vjeux", "Welcome! Please wait for a new game to start.")
+	puts "#{socket.remote_addr}:#{socket.remote_port} connected"
 
-			puts "#{socket.remote_addr}:#{socket.remote_port} connected"
+	Listener.once(socket, 'data', lambda { |data|
+		puts "Data Received #{data}"
+	})
 
-			Listener.once(socket, 'data', lambda { |data|
-				puts "Data Received #{data}"
-			})
+	Listener.add(socket, 'close', lambda { |data|
+		client.alive = false
+		puts "Closed"
+	})
 
-			Listener.add(socket, 'close', lambda { |data|
-				client.alive = false
-				puts "Closed"
-			})
+	socket.write "Hello\r\n"
+	socket.write "Start\r\n"
+})
 
-			socket.write "Hello\r\n"
-			socket.write "Start\r\n"
-		})
-	end
-
-	def sendMessage(a, b)
-		puts "send - #{a} #{b}"
-	end
-
+def sendMessage(a, b)
+	puts "send - #{a} #{b}"
 end
-
-GameTheory.new
