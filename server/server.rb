b246007bd2aa469c335c51eea2e3c23684ab45a1
Vjeux <vjeuxx@gmail.com>
2012-01-13 23:31:55 +0100

    Yield try

--- server/server.rb
+++ server/server.rb
@@ -1,10 +1,46 @@
 
+require 'fiber'
 require 'rubygems'
 require 'cool.io'
-require_relative 'listener.rb'
 
 ADDR = '127.0.0.1'
 PORT = 1337
+
+
+listeners = {}
+listeners.default_proc = lambda { |hash, key|
+	h = {}
+	h.default_proc = lambda { |hash, key|
+		hash[key] = []
+	}
+	hash[key] = h;
+}
+
+Listener_call = lambda { |key, type, data|
+	listeners[key][type].each { |listener|
+		listener[data]
+	}
+}
+
+Listener_remove = lambda { |key, type, listener|
+	listeners[key][type].delete(listener)
+}
+
+Listener_removeAll = lambda { |key, type|
+	listeners[key].delete(type)
+}
+
+Listener_add = lambda { |key, type, listener|
+	listeners[key][type] << listener
+}
+
+Listener_once = lambda { |key, type, callback|
+	listener = lambda { |data|
+		callback[data]
+		Listener_remove[key, type, listener]
+	}
+	listeners[key][type] << listener
+}
 
 
 client_id = 0
@@ -13,13 +49,13 @@ clients = []
 createServer = lambda { |addr, port, callback|
 	cool.io.server addr, port do
 		on_connect do
-			callback.call(self)
+			callback[self]
 		end
 		on_close do
-			Listener.call(self, 'close')
+			Listener_call[self, 'close']
 		end
 		on_read do |data|
-			Listener.call(self, 'data', data)
+			Listener_call[self, 'data', data]
 		end
 	end
 	cool.io.run
@@ -33,31 +69,38 @@ send = lambda { |clients, *msg|
 	}
 }
 
+root_fiber = Fiber.current
+
 query = lambda { |clients, msg, init, update, callback|
-	msg = msg.join(' ')
-	puts "> #{msg}"
 
-	n = clients.length
+	f = Fiber.new {
+		msg = msg.join(' ')
+		puts "> #{msg}"
 
-	answer = lambda {
-		n -= 1
-		if n == 0
-			callback[]
-		end
+		n = clients.length
+
+		answer = lambda {
+			n -= 1
+			if n == 0
+				Fiber.yield
+			end
+		}
+
+		clients.each { |client|
+			init[client]
+
+			Listener_once[client['socket'], 'data', lambda { |data|
+				data = data.slice(0, data.length - 2)
+				puts "< #{data}"
+				update[client, data]
+				f.resume
+				answer[]
+			}]
+
+			client['socket'].write msg + "\r\n"
+		}
 	}
-
-	clients.each { |client|
-		init[client]
-
-		Listener.once(client['socket'], 'data', lambda { |data|
-			data = data.slice(0, data.length - 2)
-			puts "< #{data}"
-			update[client, data]
-			answer[]
-		})
-
-		client['socket'].write msg + "\r\n"
-	}
+	f.resume
 }
 
 
@@ -77,15 +120,16 @@ game = lambda {
 			puts "End!"
 		}
 	]
+	puts "End2"
 }
 
 
 createServer[ADDR, PORT, lambda { |socket|
 	client = {"id" => client_id, "socket" => socket, "alive" => true}
 	client_id += 1
-	Listener.add(socket, 'close', lambda {
+	Listener_add[socket, 'close', lambda {
 		client['alive'] = false
-	})
+	}]
 	clients << client
 	send[[client], "Welcome! Please wait for a new game to start."]
 
